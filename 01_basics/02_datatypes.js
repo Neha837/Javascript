980904154c72afd82a352706ac98ed5abaddd1de
Neha837 <136150738+Neha837@users.noreply.github.com>
2024-05-21 19:58:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strick"     // treate as a newer version
+
+// alert(3+3)    // we r using nodejs not browser
+
+console.log(3
+    +
+    3)           // code readabillity should be high
+console.log("neha")
+
+// primitive datatypes
+
+let name = "neha"
+let age = 20
+let isLoggedIn = false
+let state;
+
+
+// number  => 2 to power 53
+// string  => ""
+// boolean => true or false
+// bigint
+// null    => standalone value
+// undefined 
+// symbol  => unique
+
+
+// object
+console.log(typeof null)           //object
+console.log(typeof undefined)      //undefined
